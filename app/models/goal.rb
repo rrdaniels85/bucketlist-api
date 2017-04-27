10b6c09d512b2c03a9ca5a8033f1f6b88052f975
rrdaniels85 <rachel.daniels@libertymutual.com>
2017-04-27 19:57:56 -0400

    Updated goal model to require validations
    Required validations for description, category, and status

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,7 @@
 class Goal < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
+
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :status, presence: true
 end
